364908b60320f07d5754322f1dc93782e9d12638
YUE.W <108642711+Yuegh@users.noreply.github.com>
2023-11-27 11:32:10 +0800

    '版本1'

--- blog/src/components/Layout/header/blog-header.vue
+++ blog/src/components/Layout/header/blog-header.vue
@@ -91,7 +91,7 @@ onMounted(() => {
     <div class="pc_menu flex_r_between">
       <div class="sub-avatar">
         <router-link v-if="getBlogAvatar" to="/"><el-avatar class="el-avatar" :src="getBlogAvatar" /> </router-link>
-        <!-- <MessageBox class="ml-[10px]" v-if="getUserInfo.id" :user-id="getUserInfo.id" type="pc" /> -->
+        <MessageBox class="ml-[10px]" v-if="getUserInfo.id" :user-id="getUserInfo.id" type="pc" />
       </div>
       <div class="flex_r_around">
         <BlogSearch></BlogSearch>
