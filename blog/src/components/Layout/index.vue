ef512f236150c82c855c782596fda3d862783466
YUE.W <108642711+Yuegh@users.noreply.github.com>
2023-11-27 16:19:37 +0800

    '版本修改2'

--- blog/src/components/Layout/index.vue
+++ blog/src/components/Layout/index.vue
@@ -2,9 +2,11 @@
   <div class="layout">
     <blog-header></blog-header>
     <blog-main></blog-main>
+    <blog-footer></blog-footer>
   </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
